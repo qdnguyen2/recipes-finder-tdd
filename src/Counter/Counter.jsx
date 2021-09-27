79c2df16715fd6536f61dafc6b8914cce5c21f83
Quan <qdnguyen2@yahoo.com>
2021-09-26 21:58:05 -0700

    Update production code to satisfy the unit tests

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { useState } from "react";
 function Counter() {
+  const [counterValue, setCounterValue] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
   return (
     <div>
-      <h1 data-testid="header">My Counter</h1>
+      <h1 data-testid="headerId">My Counter</h1>
+      <h2 data-testid="counterId" readOnly>
+        {counterValue}
+      </h2>
+      <button data-testid="subtractBtnId">-</button>
+      <input
+        data-testid="inputId"
+        type="number"
+        value={inputValue}
+        readOnly
+      ></input>
+      <button data-testid="addBtnId">+</button>
     </div>
   );
 }
--- src/Counter/__test__/Counter.test.js
+++ src/Counter/__test__/Counter.test.js
@@ -8,21 +8,35 @@ describe("<Counter /> component", () => {
     //const component = render(<Counter />);
     //const headerEle = component.getByTestId("header");
     const { getByTestId } = render(<Counter />);
-    const headerEle = getByTestId("header");
+    const headerEle = getByTestId("headerId");
     expect(headerEle.textContent).toBe("My Counter");
   });
 
   it("should start with text of 0", () => {
     const { getByTestId } = render(<Counter />);
-    const counterElem = getByTestId("counter");
+    const counterElem = getByTestId("counterId");
 
     expect(counterElem.textContent).toBe("0");
   });
 
-  it("should contain a textbox wiwth initial value of 1", () => {
+  it("should contain a textbox with initial value of 1", () => {
     const { getByTestId } = render(<Counter />);
     const inputElem = getByTestId("inputId");
 
     expect(inputElem.value).toBe("1");
+  });
+
+  it("should render add button with '+' sign", () => {
+    const { getByTestId } = render(<Counter />);
+    const addBtnElem = getByTestId("addBtnId");
+
+    expect(addBtnElem.textContent).toBe("+");
+  });
+
+  it("should render minus button with '-' sign", () => {
+    const { getByTestId } = render(<Counter />);
+    const subtractBtnElem = getByTestId("subtractBtnId");
+
+    expect(subtractBtnElem.textContent).toBe("-");
   });
 });
